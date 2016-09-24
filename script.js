7ac069cc77e976282bfe993aaddd0ee8e8c42da9
SmithingChick <sharon.smith.metals@gmail.com>
2016-09-23 18:33:42 -0700

    bolean 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
 if (parseInt(guess) === randomNumber ) {
   correctGuess = true;
 }
-if (correctGuess === true) {
+if ( correctGuess ) {
   document.write('<p>You guessed the number!</p>');
 } else {
   document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
